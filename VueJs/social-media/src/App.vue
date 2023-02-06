826d594e1303373e5fa3baa303adcec409abf566
Abinaya <abinaya@lumerasoft.com>
2023-02-06 21:24:07 +0530

    Create post with local storage

---
+++ VueJs/social-media/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <PostForm />
+</template>
+
+<script>
+import PostForm from './components/PostForm.vue'
+export default {
+  name: 'App',
+  components: {
+    PostForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
